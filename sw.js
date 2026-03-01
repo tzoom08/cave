cb238ddad090fcdb40946e7930f8e9e34d212a8b
tzoom08 <30917443+tzoom08@users.noreply.github.com>
2026-02-28 16:58:55 -0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cave-v5';
+const CACHE_NAME = 'cave-v9';
 const ASSETS = ['/cave/', '/cave/index.html', '/cave/manifest.json', '/cave/sw.js'];
 
 self.addEventListener('install', e => {
